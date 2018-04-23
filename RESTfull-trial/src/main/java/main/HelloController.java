d553eb4deb55a0cffa40067ee17515978968e927
KrasilnikovIS <ivankrasilnikov2@gmail.com>
2018-04-23 15:39:04 +0500

    Added different RequestMapping with value and method

--- RESTfull-trial/src/main/java/main/HelloController.java
+++ RESTfull-trial/src/main/java/main/HelloController.java
@@ -1,8 +1,11 @@
 package main;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +14,7 @@ public class HelloController {
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 	
-	@RequestMapping("/hello")
+	@RequestMapping(value="/hello", method=RequestMethod.GET)
 	public HelloModel helloModel(@RequestParam(value="name", defaultValue="world") String name) {
 		return new HelloModel(counter.incrementAndGet(), String.format(template, name));
 	}
